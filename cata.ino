e00d901f0323b057fade7e9e9dc0ca00b0a58cc6
catalinaleopea <32869277+catalinaleopea@users.noreply.github.com>
2017-12-04 22:25:17 +0200

    Update cata.ino

--- cata.ino
+++ cata.ino
@@ -1,9 +1,11 @@
-/*Leopea Catalina,grupa 243
+/*
+ Autor:Leopea Catalina,grupa 243
  
  Nume joc: Repeater
+ 
  Tip: Single player
 
-Hardware: Arduino Uno, display matrice 8x8 cu driver 7219, 4x butoane, display LCD, potentiometru de 10K ohm,
+ Hardware: Arduino Uno, display matrice 8x8 cu driver 7219, 4x butoane, display LCD, potentiometru de 10K ohm,
 4x rezistente 10K ohm, fire de legatura, 3x breadboard mare,1x breadbard mic
 
 Am asamblat componentele dupa modelele urmatoare:
@@ -11,15 +13,20 @@ Am asamblat componentele dupa modelele urmatoare:
 -butoanele https://www.arduino.cc/en/Tutorial/Button
 -display-ul LCD  https://www.arduino.cc/en/Tutorial/HelloWorld
 
-Software:
-Ideea joculului e de a afisa o secventa de sageti pe matrice, iar jucatorul sa o reproduca cu ajutorul a 4 butoane (stanga, dreapta, sus, jos).  
+ Software:
+Ideea joculului e de a afisa o secventa de sageti pe matrice, iar jucatorul sa o reproduca cu ajutorul a 4 butoane (stanga, dreapta,
+sus, jos) avand doar doua vieti.  
 Daca a introdus corect secventa trece la nivelul urmator, unde va fi generata o alta secventa de dimensiune mai mare cu o unitate.
-Jocul se termina atunci cand utilizatorul introduce o secventa gresita.
-Cu ajutorul display-ului LCD afisez un mesaj de inceput, pentru a sugera jucatorului sa apese un buton pentru a juca, apoi incepe afisarea secventei,
-urmata de un afisaj special care indica randul jucatorului si un “smiley face” daca datele au fost corecte sau “sad face” altfel.
-La sfarsitul jocului ecranul LCD va afisa un mesaj de final, dupa o apasare de buton scorul, iar inca o apasare va insemna optiunea de intoarcere la nivelul 1.
+Jocul se termina atunci cand utilizatorul introduce o secventa gresita, mai exact la prima sageata gresita din secventa.
+La primele doua greseli viata jucatorului scade si se repeta nivelul la care a gresit cu o noua secventa generata de sageti, la a treia
+greseala jocul s-a terminat.
+Cu ajutorul display-ului LCD afisez un mesaj de inceput, pentru a sugera jucatorului sa apese un buton pentru a juca, apoi incepe
+afisarea secventei, urmata de un afisaj special "GO" care indica randul jucatorului si un “smiley face” daca datele au fost corecte sau
+“sad face” altfel. La sfarsitul jocului ecranul LCD va afisa un mesaj de final, dupa o apasare de buton scorul, iar inca o apasare 
+va insemna optiunea de intoarcere la nivelul 1.
 
 Evolutie in timp:
+-doua vieti
 -nivel
 -dificultate: consta in numarul de sageti care apar pe display
        Ex:  La nivelul k apar k sageti pe display
@@ -315,21 +322,23 @@ void Menu(){
       do{                        //repeta asta pana cand jocul se termina
         lcd.clear();
         result = play();
-        if(result == 0 && lives == 2){//la prima greseala scad o viata
-          digitalWrite(lifeLed2,LOW);
+        if(result == 0){
+         if(lives == 2){//la prima greseala scad o viata
+          digitalWrite(lifeLed2, LOW);
           lives--;
           level--;
-        }
-        else
-         if(result == 0 && lives == 1){//la a doua greseala mai scad o viata
-            digitalWrite(lifeLed1,LOW);
+         }
+         else
+          if(lives == 1){//la a doua greseala mai scad o viata
+            digitalWrite(lifeLed1, LOW);
             lives--;
             level--;
           }
           else
-            if(result == 0 && lives == 0){//am gresit si nu mai am vieti
+            if(lives == 0){//am gresit si nu mai am vieti
               lives--;
             }
+        }
       }while(lives >= 0);       //se termina cand nu mai am vieti
       fail();
       lcd.clear();
